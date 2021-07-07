0da3600bddcee406843f8f2f12047cf743042b8a
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-07-07 10:54:18 +0100

    mutation example added

--- __tests__/teasChai.test.js
+++ __tests__/teasChai.test.js
@@ -1,4 +1,4 @@
-import chai from 'chai'
+import chai, { should } from 'chai'
 import chaiGraphQL from 'chai-graphql'
 chai.use(chaiGraphQL)
 
@@ -12,6 +12,7 @@ const expect = chai.expect
 //test data
 
 const teaName = "Lemon Balm"
+const teaPrice = 0.99
 
 describe('testing teas endpoint with chai graphql assertions', ()=> {
     it('should query data from teas endpoint', done =>{
@@ -32,7 +33,7 @@ describe('testing teas endpoint with chai graphql assertions', ()=> {
       request
       .post("/")
       .send({
-        query: '{ teas(name: "Lemon Balm") { id, name} }',
+        query: `{ teas(name: "${teaName}") { id, name} }`,
       })
       .set("Accept", "application/json")
       .expect(200)
@@ -74,6 +75,20 @@ describe('testing teas endpoint with chai graphql assertions', ()=> {
         expect(res.body.data.teas[0]).to.have.property('name')
         expect(res.body.data.teas[0]).to.have.property('description')
         expect(res.body.data.teas[0]).to.have.property('price')
+        done()
+      })
+    })
+    it('should be able to add new tea (mutation example)', done => {
+      request
+      .post("/")
+      .send({
+        query: `mutation { addTea(teaInput: { name: "Breakfast Tea", description: "Intensive falvour", price: ${teaPrice}, producerId: "60e569889ef12a00084fb979" }){ id name price} }`
+      })
+      .set("Accept", "application/json")
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.data.addTea.price).to.equal(teaPrice);
         done()
       })
     })
